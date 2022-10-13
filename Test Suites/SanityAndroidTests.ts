e4aa585f3b4da0211755310b33fca820d737ce1a
avinashs15 <avinashs15@gmail.com>
2022-10-13 12:39:56 +0200

    New

--- Test Suites/SanityAndroidTests.ts
+++ Test Suites/SanityAndroidTests.ts
@@ -5,10 +5,23 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6c0878aa-9879-4d5a-9f81-0d13a11e8829</testSuiteGuid>
+   <testCaseLink>
+      <guid>f273e073-1c36-4f52-b8bb-6974408a0547</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobiletest001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bbc3c7e0-3082-47ad-a681-1b409cf98896</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
