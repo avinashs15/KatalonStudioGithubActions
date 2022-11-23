0715d536bde38ef1e0ee6249653f17cb11964f5d
avinashs15 <avinashs15@gmail.com>
2022-11-23 09:47:00 +0100

    ADDED MOBBILE TEST

---
+++ Test Suites/MobileAndroid.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MobileAndroid</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0fb908c6-7e5a-41b2-968a-4aef7abe8b28</testSuiteGuid>
+   <testCaseLink>
+      <guid>e87ec567-591f-46b5-b11c-bc57303144c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobiletest001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bbc3c7e0-3082-47ad-a681-1b409cf98896</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
